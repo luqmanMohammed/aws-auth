36961a03f77a1bffc39f4bd6427a9d7f785844bc
Luqman Mohammed <m.luqman077@gmail.com>
2024-07-06 20:45:46 +0100

    feat(debug): Add eks get-token using pre-signed url instead of external cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod cache;
 mod cmd;
 mod creds;
+mod eks;
+mod oidc;
 
 use cache::CacheManager;
 use cmd::Args;
 use creds::{resolve_exec_credentials, OidcCmdResolver};
+use eks::generate_eks_auth_token;
 
-fn main() -> Result<(), ()> {
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    generate_eks_auth_token().await.map_err(|err| eprintln!("ERROR: {}", err))?;
     let args = Args::from_env_args().map_err(|err| eprintln!("ERROR: {}", err))?;
     let cache_manager = CacheManager::new(&args);
 
